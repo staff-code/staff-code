82a5acc1f366ef27eba812cbc9645c9edf184cf2
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2020-12-17 13:03:03 -0800

    to-do

--- src/package/dom/dom.ts
+++ src/package/dom/dom.ts
@@ -9,6 +9,8 @@ import {root} from "./root"
 //  - it also shouldn't need to pass root to `translateInputToDisplay`; have it fallback to the internal root
 //  - and also expose something to allow the EDO script to write the SVG to file, related to Copy Image button
 //  - it also shouldn't force you use browser stuff (document) if you don't want to
+//  - and maybe the script should add all the subdivs of the root, but not here, I mean, so we can simplify the HTML
+//  Replacement for the bbCode
 
 const buildStaffCode = (): HTMLDivElement => {
     root.appendChild(input)
