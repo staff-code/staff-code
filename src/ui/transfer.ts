80f677c58d921fb0f08490855840a66e0ce8848c
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2020-12-20 18:23:54 -0800

    v1.0.18: only retranslate to display when the last character is a whitespace

--- src/ui/transfer.ts
+++ src/ui/transfer.ts
@@ -1,11 +1,16 @@
-import {Io} from "@sagittal/general"
+import {BLANK, finalElement, Io, NEWLINE, SPACE, TAB} from "@sagittal/general"
 import {computeInputSentenceUnicode} from "../translate"
+
+const WHITESPACE_CHARS = [NEWLINE, TAB, SPACE]
 
 const transferInputToDisplay = (root: Element): void => {
     const input = root.querySelector("textarea")!
-    const display = root.querySelector("div")!
+    const value = input.value as Io
 
-    display.textContent = computeInputSentenceUnicode(input.value as Io)
+    if (!WHITESPACE_CHARS.includes(finalElement(value.split(BLANK)))) return
+
+    const display = root.querySelector("div")!
+    display.textContent = computeInputSentenceUnicode(value)
 }
 
 export {
--- src/ui/variants/package/input.ts
+++ src/ui/variants/package/input.ts
@@ -2,9 +2,6 @@ import {doOnNextEventLoop, Io, Ms} from "@sagittal/general"
 import {Code} from "../../../translate"
 import {transferInputToDisplay} from "../../transfer"
 
-// TODO: FEATURE IMPROVE, BLOCKED: OPEN OR CLOSED INITIAL STAFF?
-//  Should include a barline before the clef
-//  Waiting for Dave's reply
 const INITIAL_STAFF_CODE_INPUT_VALUE = `${Code[Code[`st`]]} ${Code[Code[`tbcf`]]} ${Code[Code[`;`]]} ` as Io
 
 let keydown = false
@@ -14,17 +11,7 @@ const buildPackageInput = (root: HTMLDivElement): HTMLTextAreaElement => {
     input.value = INITIAL_STAFF_CODE_INPUT_VALUE
     input.addEventListener("keydown", (): void => {
         keydown = true
-        doOnNextEventLoop((): void => {
-            // TODO: BUG, READY TO GO: THIS SHOULD NOT FIRE IF YOU'RE HOLDING DOWN CTRL, ABOUT TO PASTE
-            //  I think it should only fire if it's any character that might possibly be a code
-            //  Or wait, maybe it only fires if it's a space! Then we don't even have to do special parsing
-            //  - Here's the old task for "do not re-render until pushing a space"
-            //  - But then you have to remember to add a space to the BBCode variant at the end
-            //  Or it won't render the last codeword
-            //  - But make sure that plays well with the terminal Smart Advance `inputWords.push(Code[Code[`;`]])` below
-            //  - Oh yeah, and it ought to be space, tab, or return, since all whitespace really should trigger it
-            transferInputToDisplay(root)
-        }, 100 as Ms).then()
+        doOnNextEventLoop((): void => {transferInputToDisplay(root)}, 100 as Ms).then()
     })
     input.addEventListener("keyup", (): void => {
         keydown = false
