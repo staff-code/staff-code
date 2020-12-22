cd61ad7a61acf550c8823ac5c5b51f5f9c3bf034
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2020-12-21 17:51:12 -0800

    v1.0.26: resize input

--- src/ui/input.ts
+++ src/ui/input.ts
@@ -1,10 +1,10 @@
-const ABOUT_FORTY_CHARS_WIDE_PX = 200
-const PHI = (1 + Math.sqrt(5)) / 2
-const ABOUT_THIRTY_LINES_HIGH_PX = ABOUT_FORTY_CHARS_WIDE_PX * PHI
+const ABOUT_FORTY_CHARS_WIDE_PX = 300
+const ABOUT_THIRTY_LINES_HIGH_PX = 250
 
 const setupInput = (input: HTMLTextAreaElement): void => {
     input.style.width = `${ABOUT_FORTY_CHARS_WIDE_PX}px`
     input.style.height = `${ABOUT_THIRTY_LINES_HIGH_PX}px`
+    input.style.resize = "both"
 }
 
 export {
