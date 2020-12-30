9d04d6d8dead5e817e51d76fad8fca488a3cbd0a
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2020-12-30 14:08:15 -0800

    add to-do

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,5 @@ export {
     transferInputToDisplay, setupPackageRoot, loadFontsThen, downloadSvg, StaffCodeOptions, StaffCodeCallback,
 } from "./ui"
 export {computeUnicodeLiteral, Octals, SMuFL_MAP, Unicode, computeInputSentenceUnicode} from "./translate"
+
+// TODO: add to readme about how you need to update staff code and then app and also forum
