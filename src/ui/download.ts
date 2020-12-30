7b7d4b31b6aa333ae98c7a382652eddf05c6774b
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2020-12-29 18:21:34 -0800

    v1.0.58: svg width scaling

--- src/ui/download.ts
+++ src/ui/download.ts
@@ -1,40 +1,91 @@
-import {BLANK, FontName, vectorizeText} from "@sagittal/general"
+import {BLANK, computeLineCount, FontName, Px, vectorizeText} from "@sagittal/general"
 import {Unicode} from "../translate"
 import {DEFAULT_LINE_HEIGHT_MULTIPLIER} from "./display"
-import {computeSvgHeight} from "./svgHeight"
+
+// TODO: FEATURE IMPROVE, BLOCKED: SVG SIZE
+//  If you have a very low leger note, it gets cut off in the SVG
+//  Okay, weird, for some reason now it's not cutting off anymore, but it is shrinking everything...
+//  Or maybe it's just Chrome randomly scaling things.
+//  Probably I should pause this until I finish debugging opentype.js with injected debug helpers for
+//  Converting their decimal unicode code points to hexadecimal. (see the edoStaves repo)
+//  In case we end up going with it and it has its own mysterious SVG rendering issues
+
+const HEIGHT_WHICH_CAUSES_SVG_TO_MATCH_TEXT: Px = 57 as Px
+const APPARENT_SCALING_EXPONENT = 1.16
 
 const DOWNLOAD_FILENAME: string = "staffCode.svg"
 
-// TODO: FEATURE IMPROVE, TOUGH AND LOW PRIORITY: SVG SIZE
-//  - programmatically determine its total width, whatever needed to match contents, not just safe but huge
-//  - if you have a very low leger note, it gets cut off in the SVG
+const computeSvgHeight = (unicodeSentence: Unicode): Px =>
+    HEIGHT_WHICH_CAUSES_SVG_TO_MATCH_TEXT * (computeLineCount(unicodeSentence) ** APPARENT_SCALING_EXPONENT) as Px
 
-const downloadSvg = (display: HTMLElement, svg: SVGElement): void => {
+const scaleSVGToFitContents = (svg: SVGGraphicsElement): void => {
+    const bbox = svg.getBBox()
+
+    svg.setAttribute("width", `${bbox.x + bbox.width + bbox.x}`)
+    svg.setAttribute("height", `${bbox.y + bbox.height + bbox.y}`)
+}
+
+const buildHiddenButAddedToDOMSvgWhoseContentsSizeCanBeMeasuredInOrderToScaleItToFitThem = (): SVGGraphicsElement => {
+    const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg") as Element as SVGGraphicsElement
+    svg.style.position = "absolute"
+    svg.style.top = "-9999px"
+    svg.style.left = "-9999px"
+
+    svg.setAttribute("xmlns", "http://www.w3.org/2000/svg")
+    svg.setAttribute("xmlns:xlink", "http://www.w3.org/1999/xlink")
+
+    document.body.appendChild(svg)
+
+    return svg
+}
+
+const cloneANonHiddenSoItCanBeSeenButNotAddedToDOMSvgNowThatItHasBeenScaled = (
+    svg: SVGGraphicsElement,
+): SVGGraphicsElement => {
+    const clonedSvg = svg.cloneNode(true) as SVGGraphicsElement
+    clonedSvg.style.position = "relative"
+    clonedSvg.style.top = "0"
+    clonedSvg.style.left = "0"
+
+    return clonedSvg
+}
+
+const buildDummyDownloadLinkAndClickIt = (svgBlobURL: string): void => {
+    const a: HTMLAnchorElement = document.createElement("a")
+
+    a.style.display = "none"
+    a.href = svgBlobURL
+    a.download = DOWNLOAD_FILENAME
+
+    a.click()
+    window.URL.revokeObjectURL(svgBlobURL)
+}
+
+const buildSvgBlobUrl = (clonedSvg: SVGGraphicsElement): string => {
+    const outerHTML = clonedSvg.outerHTML
+    const blob = new Blob([outerHTML], {type: "image/svg+xml;charset=utf-8"})
+    const URL = window.URL || window.webkitURL || window
+
+    return URL.createObjectURL(blob)
+}
+
+const downloadSvg = (display: HTMLElement): void => {
     const unicodeSentence: Unicode = (display.textContent || BLANK) as Unicode
-
-    const height = computeSvgHeight(unicodeSentence)
-    svg.setAttribute("height", `${height}`)
-
     const options = {
-        height,
+        height: computeSvgHeight(unicodeSentence),
         font: "Bravura Text BB" as FontName,
         lineSpacing: DEFAULT_LINE_HEIGHT_MULTIPLIER,
         styletags: {breaklines: true},
     }
+
+    const svg = buildHiddenButAddedToDOMSvgWhoseContentsSizeCanBeMeasuredInOrderToScaleItToFitThem()
     svg.innerHTML = vectorizeText(unicodeSentence, options)
+    scaleSVGToFitContents(svg)
+    const clonedSvg = cloneANonHiddenSoItCanBeSeenButNotAddedToDOMSvgNowThatItHasBeenScaled(svg)
 
-    const outerHTML = svg.outerHTML
-    const blob = new Blob([outerHTML], {type: "image/svg+xml;charset=utf-8"})
-    const URL = window.URL || window.webkitURL || window
-    const blobURL = URL.createObjectURL(blob)
-    const a: HTMLAnchorElement = document.createElement("a")
+    const svgBlobUrl = buildSvgBlobUrl(clonedSvg)
 
-    a.style.display = "none"
-    a.href = blobURL
-    a.download = DOWNLOAD_FILENAME
-
-    a.click()
-    window.URL.revokeObjectURL(blobURL)
+    buildDummyDownloadLinkAndClickIt(svgBlobUrl)
 }
 
 export {
