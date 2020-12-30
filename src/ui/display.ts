0d902ff350e2313dd2e2ec0a5e02917bad51aed7
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2020-12-29 16:36:45 -0800

    deal with negative left side bearing by adding a little horizontal margin

--- src/ui/display.ts
+++ src/ui/display.ts
@@ -5,12 +5,13 @@ const FONT_SIZE = 2.5 as Em
 const INLINE_FONT_SIZE = 1 as Em
 const DEFAULT_LINE_HEIGHT_MULTIPLIER = 2 as Multiplier<Em>
 const MARGIN_SIZE = 1 as Em
+const HORIZONTAL_MARGIN_MULTIPLIER = 1/8 as Multiplier<Em>
 
 const buildDisplay = (inline: boolean = false, {font, lineHeight}: StaffCodeOptions = {}): HTMLElement => {
     const display = document.createElement(inline ? "span" : "div")
     display.style.fontSize = `${inline ? INLINE_FONT_SIZE : FONT_SIZE}em`
     display.style.lineHeight = `${lineHeight || DEFAULT_LINE_HEIGHT_MULTIPLIER}`
-    display.style.margin = `${MARGIN_SIZE}em 0`
+    display.style.margin = `${MARGIN_SIZE}em ${MARGIN_SIZE * HORIZONTAL_MARGIN_MULTIPLIER}em`
     display.style.fontFamily = font || "Bravura Text BB"
     display.style.whiteSpace = "pre"
     display.classList.add("display")
