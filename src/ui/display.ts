a7f3e21e18322390c675c5911fba67d5704b5653
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2021-01-01 12:55:54 -0800

    v1.0.66: forgot no margin, and scale by 100

--- src/ui/display.ts
+++ src/ui/display.ts
@@ -8,13 +8,13 @@ const MARGIN_SIZE = 1 as Em
 const HORIZONTAL_MARGIN_MULTIPLIER = 1 / 8 as Multiplier<Em>
 
 const buildDisplay = (options: StaffCodeOptions = {}): HTMLElement => {
-    const {font, lineHeight, inline = false, size = 1 as Decimal<{integer: true}>} = options
+    const {font, lineHeight, inline = false, size = 100 as Decimal<{integer: true}>} = options
 
     const display = document.createElement(inline ? "span" : "div")
     if (inline) display.style.display = "inline-block"
-    display.style.fontSize = `${size * (inline ? INLINE_FONT_SIZE : FONT_SIZE)}em`
+    display.style.fontSize = `${(size / 100) * (inline ? INLINE_FONT_SIZE : FONT_SIZE)}em`
     display.style.lineHeight = `${lineHeight || DEFAULT_LINE_HEIGHT_MULTIPLIER}`
-    display.style.margin = `${MARGIN_SIZE}em ${MARGIN_SIZE * HORIZONTAL_MARGIN_MULTIPLIER}em`
+    if (!inline) display.style.margin = `${MARGIN_SIZE}em ${MARGIN_SIZE * HORIZONTAL_MARGIN_MULTIPLIER}em`
     display.style.fontFamily = font || "Bravura Text BB"
     display.style.whiteSpace = "pre"
     display.classList.add("display")
