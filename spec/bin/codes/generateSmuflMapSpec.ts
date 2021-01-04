d91a098ad0dcd4559b073ae7e3a3dff2b9e9a1c1
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2021-01-03 16:54:41 -0800

    refine test explanation

--- spec/bin/codes/generateSmuflMapSpec.ts
+++ spec/bin/codes/generateSmuflMapSpec.ts
@@ -3,18 +3,8 @@ import {generateSmuflMap} from "../../../bin/codes/generateSmuflMap"
 import {Code, computeUnicodeLiteral, Unicode} from "../../../src"
 import {SMuFL_ABBREVIATION_ALIASES_MAP} from "../../../src/translate/codes/aliases"
 
-// TODO: CLEAN, READY TO GO: ABBREVIATED CODES INFRASTRUCTURE
-//  THe only thing I have to do left is figure out how exact I want to handle the generation of JSON/hardcode/whatever
-//  Like... now that I pulled this in, and used it to prove out the generation, do I even need to keep it?
-//  I could keep it as a test expectation, and it would fail whenever they changed SMuFL on us.
-//  That's probably fine. Just rename everything accordingly. You can even keep all the formatting and comments and such
-//  But just in the test.
-//  And then just make sure that the generate script writes to file, like the other script does, and puts it where the
-//  Original file used to be.
-//  Then make a package.json script for it and make sure that runs the same time the other script runs.
-
 describe("generateSmuflMap", (): void => {
-    it("matches the hardcoded map", (): void => {
+    it("should fail whenever SMuFL adds or changes a glyph name, to alert us that we need to add both a new code and an abbreviation", (): void => {
         onlyRunInCi()
 
         const actual = generateSmuflMap()
@@ -22,15 +12,15 @@ describe("generateSmuflMap", (): void => {
         let totalFailureCount = 0
 
         const actualEntries = Object.entries(actual) as Array<[Code & Word, Unicode & Word]>
-        const hardcodedSmuflMapEntries =
+        const smuflAbbreviationAliasesMapEntries =
             Object.entries(SMuFL_ABBREVIATION_ALIASES_MAP) as Array<[Code & Word, Unicode & Word]>
-        expect(actualEntries.length).toBe(hardcodedSmuflMapEntries.length)
+        expect(actualEntries.length).toBe(smuflAbbreviationAliasesMapEntries.length)
 
-        if (hardcodedSmuflMapEntries.length > actualEntries.length) {
-            hardcodedSmuflMapEntries.forEach(
-                ([hardcodedCode, hardcodedUnicode]: [Code & Word, Unicode & Word]): void => {
-                    if (isUndefined(actual[hardcodedCode])) {
-                        fail(`The hardcoded SMuFL map has an extra code, ${hardcodedCode}, which maps to ${computeUnicodeLiteral(hardcodedUnicode)}.`)
+        if (smuflAbbreviationAliasesMapEntries.length > actualEntries.length) {
+            smuflAbbreviationAliasesMapEntries.forEach(
+                ([expectedCode, expectedUnicode]: [Code & Word, Unicode & Word]): void => {
+                    if (isUndefined(actual[expectedCode])) {
+                        fail(`The current Staff Code SMuFL map has a code, ${expectedCode}, which maps to ${computeUnicodeLiteral(expectedUnicode)}, but which is not included in the latest SMuFL release.`)
                         totalFailureCount += 1
                     }
                 },
@@ -41,10 +31,10 @@ describe("generateSmuflMap", (): void => {
             const expectedUnicode = SMuFL_ABBREVIATION_ALIASES_MAP[actualCode]
 
             if (isUndefined(expectedUnicode)) {
-                fail(`${actualCode} was not defined in the hardcoded SMuFL map.`)
+                fail(`${actualCode} is in the latest SMuFL release, but does not yet have a definition in Staff Code.`)
                 totalFailureCount += 1
             } else if (actualUnicode !== expectedUnicode) {
-                fail(`${actualCode}, according to the hardcoded SMuFL map, was supposed to map to ${computeUnicodeLiteral(expectedUnicode)} but instead mapped to ${computeUnicodeLiteral(actualUnicode)}.`)
+                fail(`${actualCode}, according to the current Staff Code SMuFL map, was supposed to map to ${computeUnicodeLiteral(expectedUnicode)} but in the latest SMuFL release mapped to ${computeUnicodeLiteral(actualUnicode)}.`)
                 totalFailureCount += 1
             }
         })
