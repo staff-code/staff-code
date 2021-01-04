ad9d9fa7868f219489594353c352d67360bd4aff
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2021-01-04 14:59:18 -0800

    update to-dos

--- src/index.ts
+++ src/index.ts
@@ -13,10 +13,16 @@ export {
 } from "./translate"
 
 // TODO: NEW FEATURE, BLOCKED: SYMBOL BROWSER REFERENCE
-//  Blocked because we're still discussing whether it should be an expandable section or a separate page
+//  - An expandable section (no separate page version)
+//  - there's either a click-to-copy-and-insert button, or two separate buttons
+//  - there's maybe a way to make it auto-expand via a query param
+//  - do you want it below, or to the side?
+//  - then remove the full glyphNames from the codes that it processes
+//  - could chunk things so that the reference code only loads upon expanding
 
-// TODO: FEATURE ADJUST, READY TO GO: REMOTE HOSTING
-//  So we could use SC anywhere (e.g. in GitBook)
-//  And forum admins don't have to install fonts or JS (update README.txt accordingly)
-
-// TODO: there might be some other thoughts I want to act on in that recent big forum post I made re: expandable section
+// TODO: NEW FEATURE, BLOCKED: SHARE STAFF CODES VIA LINKS
+//  This reminds me of a potential feature which had occurred to me but I don't think I ever suggested,
+//  Which is the ability to share a link to the StaffCode web app with query params in the URL
+//  Such that you'd visit the link and some particular code sentence would already be in the input and display
+//  There'd have to be a share button on the page you'd click to get the link
+//  But is that insufficiently valuable, when you can instead just share the code sentence and have someone paste it in?
