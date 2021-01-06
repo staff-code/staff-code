55379e0ad4f406e6a5575ae5c8a9478c17437ed8
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2021-01-06 12:33:01 -0800

    fight scaling issue on phpBB

--- src/ui/variants/package/index.ts
+++ src/ui/variants/package/index.ts
@@ -1,6 +1,1 @@
 export {setupPackageRoot} from "./root"
-
-// TODO: BUG, READY TO GO: [STAFF] SCALING ISSUE
-//  But the comparison between [_]sc] and [_]staff] has now brought to my attention that the output of [_]staff]
-//  Is only 7/8ths of the size it should be. i.e. there are only 7 pixels between staff-line centres
-//  When there should be 8. The web app is fine. It has the expected 8 pixels.
