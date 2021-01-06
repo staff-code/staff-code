ce080f436612b84b0c2a7d901c55a86878b938ef
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2021-01-06 12:17:45 -0800

    v1.0.88: refine versioning and remoting

--- src/ui/variants/bbCode/index.ts
+++ src/ui/variants/bbCode/index.ts
@@ -18,10 +18,15 @@ Text, and its precursors Bravura and SMuFL. For more information see https://www
 import {loadFontsThen} from "../../fonts"
 import {setupBBCodeStaffCode} from "./setup"
 
+// Just an FYI. This works (the document.write in the htmlReplacement) but it gives a warning.
+// tslint:disable max-line-length
+/*
+posting.php?mode=reply&f=17&t=436:313 A parser-blocking, cross site (i.e. different eTLD+1) script, https://sagittal.github.io/staffCode.js, is invoked via document.write. The network request for this script MAY be blocked by the browser in this or a future page load due to poor network connectivity. If blocked in this page load, it will be confirmed in a subsequent console message. See https://www.chromestatus.com/feature/5718547946799104 for more details.
+*/
 (globalThis as unknown as {staffCode: boolean}).staffCode = true
 
 loadFontsThen(setupBBCodeStaffCode)
 
 // @ts-ignore
 // tslint:disable-next-line no-console
-console.warn("Version " + __VERSION__)
+console.log("Running StaffCode v" + __VERSION__)
