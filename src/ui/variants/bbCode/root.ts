b6d48642f311f404e18b435f45ed4492aa373cf8
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2021-01-08 14:26:31 -0800

    v1.0.105: actually remove all references to reference in the bbCode variant

--- src/ui/variants/bbCode/root.ts
+++ src/ui/variants/bbCode/root.ts
@@ -1,8 +1,7 @@
 import {buildDisplay} from "../../display"
 import {DEFAULT_FONT} from "../../fonts"
-import {computeInitialCodes, computeInitialLine, computeInitialReferenceOpen, computeInitialSize} from "../../initial"
+import {computeInitialCodes, computeInitialLine, computeInitialSize} from "../../initial"
 import {setupInput} from "../../input"
-import {buildReferenceLink} from "../../reference"
 import {transferInputToDisplay} from "../../transfer"
 import {StaffCodeOptions} from "../../types"
 
@@ -11,13 +10,11 @@ const setupBBCodeRoot = (root: HTMLSpanElement, options: StaffCodeOptions = {}):
         ui: {
             inline = false,
             interactive = false,
-            reference = false,
         } = {},
         initial: {
             codes: initialCodes = computeInitialCodes(),
             size: initialSize = computeInitialSize(),
             line: initialLine = computeInitialLine(),
-            referenceOpen: initialReferenceOpen = computeInitialReferenceOpen(),
         } = {},
         font = DEFAULT_FONT,
         callback,
@@ -29,18 +26,9 @@ const setupBBCodeRoot = (root: HTMLSpanElement, options: StaffCodeOptions = {}):
     const input: HTMLTextAreaElement = root.querySelector(".input") as HTMLTextAreaElement
     const display = buildDisplay({font, inline, initialLine, initialSize})
 
-    let referenceLink
-    if (reference) {
-        referenceLink = buildReferenceLink(root, input, {callback, initialReferenceOpen})
-    }
-
     setupInput(input, root, {interactive, initialCodes, callback})
 
     root.appendChild(display)
-
-    if (referenceLink) {
-        root.appendChild(referenceLink)
-    }
 
     transferInputToDisplay(root, {callback})
 }
