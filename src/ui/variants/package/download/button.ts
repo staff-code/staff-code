b78735b0a1ec5ddcb47d40ca4c3ce41bd2945c79
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2021-01-10 11:10:27 -0800

    use fa icon

--- src/ui/variants/package/download/button.ts
+++ src/ui/variants/package/download/button.ts
@@ -2,7 +2,7 @@ import {downloadSvg} from "./svg"
 
 const buildDownloadButton = (): HTMLButtonElement => {
     const downloadButton = document.createElement("button")
-    downloadButton.textContent = "\u21e9 image" // TODO: we should try the FontAwesome: fas fa-download &#xf019;.
+    downloadButton.innerHTML = `<img style="height: 1em; vertical-align: middle" src="https://raw.githubusercontent.com/Keyamoon/IcoMoon-Free/master/SVG/200-download3.svg"/> image`
     downloadButton.style.margin = "2px"
     downloadButton.style.cursor = "pointer"
     downloadButton.style.fontFamily = "sans-serif"
--- src/ui/variants/package/reference/index.ts
+++ src/ui/variants/package/reference/index.ts
@@ -1,3 +1,1 @@
 export {buildReferenceExpander} from "./expander"
-
-// TODO: Reference should fit in mobile
