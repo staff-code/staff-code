eaea59f5c087e04379de876eb1384b37f63d705c
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2021-01-12 14:48:41 -0800

    spacing and proportions in reference

--- src/ui/variants/package/reference/css.ts
+++ src/ui/variants/package/reference/css.ts
@@ -1,15 +1,5 @@
 import {css} from "@sagittal/general"
 
-// TODO: Can we reduce the height of all rows by 4 pixels (and hence the height of the Reference by 16 times that)?
-//  I'll be happy if Gcl8vb and Gcl15ma (and nhrnblsllg and nhrnwtsllg) have at least one pixel between them.
-//  - (Also) I notice that the scrollable part of the reference field is now 22.5 rows high on my phone
-//  (21 on my desktop). If it was only 18 rows high, it would fit on my phone screen at the same time as the input field
-//  (and the browser guff at the top) and therefore cause no scrolling.
-//  Okay I’ll reduce the height and we’ll see how it looks then.
-
-// TODO: I think we need at least one more pixel of left and right padding in the code and mnemonic cells.
-//  This need shows up most in the Combining staff positions table.
-
 // TODO: I suppose it's too much to ask that only the triangle, and possibly the word "Reference",
 //  Cause the Reference to expand and collapse? Not the word StaffCode and not the blank space in between.
 //  Also, I'm pretty sure that as long as it's this way, you can't see the alt-text for StaffCode on mobile.
@@ -62,7 +52,7 @@ const REFERENCE_CSS = css`
   }
 
   details[open] {
-    height: 520px;
+    height: 432px;
   }
 
   .sc-about {
@@ -136,29 +126,33 @@ const REFERENCE_CSS = css`
     padding-right: 1.33em;
   }
 
-  /* glyph */
+  /* glyph cell */
 
   td:nth-child(1) {
     padding: 2px 5px;
     font-family: Bravura Text BB;
-    font-size: 1.875em; /* 3/4 * default 2.5 when size = 1 */
+    font-size: 1.6666em;
     width: 1em;
     text-align: center;
-    line-height: 0.9; /* so that they don't massively increase height of the rows */
+    line-height: 0.75; /* so that they don't massively increase height of the rows */
   }
 
-  /* code */
+  /* code cell */
 
   td:nth-child(2) {
     font-family: Roboto Mono;
     border: 1px solid;
+    font-size: 13.333px;
+    padding: 0 3px;
   }
 
-  /* mnemonic */
+  /* mnemonic cell */
 
   td:nth-child(3) {
     font-family: Roboto;
     border: 1px solid;
+    font-size: 13.333px;
+    padding: 0 3px;
   }
 `
 
