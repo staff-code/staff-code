2e9861e29da0f34a74dd279577b31aff7629a8ca
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2021-01-12 10:32:37 -0800

    trying to diagnose web app url matching on new url

--- src/ui/variants/package/copyLink/app.ts
+++ src/ui/variants/package/copyLink/app.ts
@@ -1,8 +1,13 @@
+import {Link} from "@sagittal/general"
 import {WEB_APP_URL} from "../../../constants"
 
+const computeBaseUrl = (location: Location): Link =>
+    location.protocol + "//" + location.host + "/" + location.pathname as Link
+
 const codeRunningInStaffCodeWebApp = (): boolean => {
-    const getUrl = window.location
-    const baseUrl = getUrl.protocol + "//" + getUrl.host + "/" + getUrl.pathname.split("/")[1]
+    const baseUrl = computeBaseUrl(window.location)
+
+    console.log("baseUrl", baseUrl, "webAppUrl", WEB_APP_URL, "window.location", window.location)
 
     return baseUrl === WEB_APP_URL
 }
