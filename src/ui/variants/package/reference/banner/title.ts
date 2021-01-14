0386cb9cc996e4cfd6f65afbc6b7f9d5fea73608
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2021-01-14 13:51:10 -0800

    tooltiptodo

--- src/ui/variants/package/reference/banner/title.ts
+++ src/ui/variants/package/reference/banner/title.ts
@@ -8,6 +8,13 @@ and Dave Keenan<br>
 <a target="_blank" href='${INTRO_TO_STAFF_CODE_LINK}'>Intro to StaffCode</a>
 `
 
+// TODO: Fat-fingers here, finds that it's easy to accidentally go to the Intro to StaffCode just by tapping the word
+//  StaffCode to bring up the tooltip. A blank line at the end of the tooltip would fix that.
+//  I also think the link should now use the full text "Introduction to StaffCode" in both places
+//  (tooltip and Reference), now that it doesn't have to compete for space with a "Back to to top" link.
+//  - And, in the tooltip, I think "StaffCode version 1.0.XXX" should be either (a) all on one line or
+//  (b) have the line-break between "StaffCode" and "version 1.0.XXX".
+
 const buildSiteTitle = (): HTMLSpanElement => {
     const siteTitle = document.createElement("span")
     siteTitle.classList.add("sc-title")
