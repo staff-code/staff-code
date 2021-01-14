729bfc333de5f58c3f70a8ea0f445724631e255e
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2021-01-14 14:13:15 -0800

    fix copy link to URL bars, hopefully for the last time

--- src/ui/variants/package/copyLink/app.ts
+++ src/ui/variants/package/copyLink/app.ts
@@ -4,15 +4,12 @@ import {WEB_APP_URL} from "../../../constants"
 const computeBaseUrl = (location: Location): Link =>
     location.protocol + "//" + location.host as Link
 
+const WWW_SUBDOMAINED_WEB_APP_URL = "https://www.staffcode.org" as Link
+
 const codeRunningInStaffCodeWebApp = (): boolean => {
     const baseUrl = computeBaseUrl(window.location)
 
-    // TODO: The fix for the copy link button updating the URL still didn't work.
-    //  I'll need to do some dedicated debugging.
-
-    console.log("diagnosing base app url checking", baseUrl, WEB_APP_URL, baseUrl === WEB_APP_URL)
-
-    return baseUrl === WEB_APP_URL
+    return baseUrl === WEB_APP_URL || baseUrl === WWW_SUBDOMAINED_WEB_APP_URL
 }
 
 export {
