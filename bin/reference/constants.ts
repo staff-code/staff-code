26ce7123d5fa6881eff726e29cc7a449d14ff916
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2021-01-13 16:04:19 -0800

    fix intro to staff code link

--- bin/reference/constants.ts
+++ bin/reference/constants.ts
@@ -1,7 +1,7 @@
 import {Link} from "@sagittal/general"
 
 // Temporary link, until a public post is ready.
-const INTRODUCTION_TO_STAFF_CODE_LINK: Link = "https://forum.sagittal.org/viewtopic.php?p=3192" as Link
+const INTRODUCTION_TO_STAFF_CODE_LINK: Link = "https://forum.sagittal.org/viewtopic.php?p=3192#p3192" as Link
 
 export {
     INTRODUCTION_TO_STAFF_CODE_LINK,
