12fceaf3355f75c531d7494f96511713dcbd7c26
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2021-01-14 09:21:26 -0800

    spinner mins and maxes

--- src/ui/variants/package/spinners/spinners.ts
+++ src/ui/variants/package/spinners/spinners.ts
@@ -1,8 +1,12 @@
-import {lowerCaseToUpperCase} from "@sagittal/general"
+import {Em, lowerCaseToUpperCase, Max, Multiplier} from "@sagittal/general"
 import {components, staffCodeConfig} from "../globals"
 import {Initial} from "../types"
 
-const buildSpinnerWrapper = (initial: Initial, handleSpinnerChange: (event: Event) => void): HTMLSpanElement => {
+const buildSpinnerWrapper = (
+    initial: Initial,
+    handleSpinnerChange: (event: Event) => void,
+    max: Max<Multiplier<Em>>,
+): HTMLSpanElement => {
     const spinnerWrapper = document.createElement("div")
     spinnerWrapper.classList.add("sc-spinner")
 
@@ -13,7 +17,8 @@ const buildSpinnerWrapper = (initial: Initial, handleSpinnerChange: (event: Even
     const spinner = document.createElement("input")
     spinner.type = "number"
     spinner.step = "0.1"
-    spinner.min = "0"
+    spinner.min = "0.1"
+    spinner.max = `${max}`
     // @ts-ignore
     spinner.value = `${staffCodeConfig.initial[initial]}`
     spinner.id = initial
--- src/ui/variants/package/spinners/wrappers.ts
+++ src/ui/variants/package/spinners/wrappers.ts
@@ -1,12 +1,13 @@
+import {Em, Max, Multiplier} from "@sagittal/general"
 import {Initial} from "../types"
 import {handleLineSpinnerChange, handleSizeSpinnerChange} from "./handlers"
 import {buildSpinnerWrapper} from "./spinners"
 
 const buildSizeSpinnerWrapper = (): HTMLSpanElement =>
-    buildSpinnerWrapper(Initial.SIZE, handleSizeSpinnerChange)
+    buildSpinnerWrapper(Initial.SIZE, handleSizeSpinnerChange, 10 as Max<Multiplier<Em>>)
 
 const buildLineSpinnerWrapper = (): HTMLSpanElement =>
-    buildSpinnerWrapper(Initial.LINE, handleLineSpinnerChange)
+    buildSpinnerWrapper(Initial.LINE, handleLineSpinnerChange, 20 as Max<Multiplier<Em>>)
 
 export {
     buildSizeSpinnerWrapper,
