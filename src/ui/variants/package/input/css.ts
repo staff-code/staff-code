33f25152bf5ecdfad032892c8bdb5a026bcb0069
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2021-01-18 10:03:38 -0800

    attempt to fix textarea ugliness in firefox

--- src/ui/variants/package/input/css.ts
+++ src/ui/variants/package/input/css.ts
@@ -21,6 +21,7 @@ const INPUT_CSS = `
         max-width: ${MAX_APP_WIDTH}px;
         resize: vertical;
         margin-bottom: 5px;
+        padding: 0;
         min-height: ${ABOUT_THREE_LINES_HIGH_PX}px;
     }
 
