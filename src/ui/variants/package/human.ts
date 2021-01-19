948ee4a11e879ea20f9937d7398c61918e536261
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2021-01-19 11:14:49 -0800

    improve human readability of codes

--- src/ui/variants/package/human.ts
+++ src/ui/variants/package/human.ts
@@ -7,6 +7,9 @@ const prepareCodesToBeHumanReadableAsEncodedQueryParams = (
     inputSentence
         .replace(/-/g, "--")
         .replace(/([^ \-]) (?=[^ \-]|$)/g, "$1-")
+        .replace(/z/g, "zz")
+        .replace(/br;/g, "\n")
+        .replace(/([^\nz])\n(?=[^\n]|$)/g, "$1z")
         .replace(/q/g, "qq")
         .replace(/([^;q]);(?=[^;q]|$)/g, "$1q") as EncodedCode & Sentence
 
@@ -16,16 +19,10 @@ const undoPreparationOfCodesToBeHumanReadableAsEncodedQueryParams = (
     encodedSentence
         .replace(/([^q])q(?=[^q]|$)/g, "$1;")
         .replace(/qq/g, "q")
+        .replace(/([^z])z(?=[^z]|$)/g, "$1br;")
+        .replace(/zz/g, "z")
         .replace(/([^\-])-(?=[^\-]|$)/g, "$1 ")
         .replace(/--/g, "-") as Io & Sentence
-
-// TODO: FEATURE IMPROVE, HUMAN CODES
-//  We could further prettify the "codes=" part of the URLs by:
-//  Replace all "z"s with "zz".
-//  Replace all newlines that are not preceded or followed by either a newline or a "z", with "z".
-//  To reverse:
-//  Replace all "z"s that are not preceded or followed by a "z", with newline.
-//  Replace all "zz"s with "z"
 
 export {
     prepareCodesToBeHumanReadableAsEncodedQueryParams,
