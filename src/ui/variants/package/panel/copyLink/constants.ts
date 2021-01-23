c8559ca805a3df0a851712e6e8841dd9836c8415
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2021-01-23 13:31:27 -0800

    fix tests for web app url

--- src/ui/variants/package/panel/copyLink/constants.ts
+++
@@ -1,7 +0,0 @@
-import {codeRunningInStaffCodeWebApp} from "./app"
-
-const CODE_RUNNING_IN_WEB_APP = codeRunningInStaffCodeWebApp()
-
-export {
-    CODE_RUNNING_IN_WEB_APP,
-}
--- src/ui/variants/package/panel/copyLink/message.ts
+++ src/ui/variants/package/panel/copyLink/message.ts
@@ -1,11 +1,11 @@
 import {BLANK} from "@sagittal/general"
 import {components} from "../../globals"
-import {CODE_RUNNING_IN_WEB_APP} from "./constants"
+import {codeRunningInStaffCodeWebApp} from "./app"
 
 const buildCopyLinkMessage = (): HTMLDivElement => {
     const copyLinkMessage = document.createElement("div")
     copyLinkMessage.classList.add("sc-copy-link-message")
-    copyLinkMessage.innerHTML = `Link copied to clipboard${CODE_RUNNING_IN_WEB_APP ? "<br>Browser URL updated" : BLANK}`
+    copyLinkMessage.innerHTML = `Link copied to clipboard${codeRunningInStaffCodeWebApp() ? "<br>Browser URL updated" : BLANK}`
     components.copyLinkMessage = copyLinkMessage
 
     return copyLinkMessage
