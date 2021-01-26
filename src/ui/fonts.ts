a60caf77430929fc757debba0cef7919cd35079c
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2021-01-26 08:17:59 -0800

    fix ci by using dynamic import of local fonts

--- src/ui/fonts.ts
+++ src/ui/fonts.ts
@@ -1,17 +1,16 @@
 // tslint:disable no-reaching-imports
-// @ts-ignore
-import bravuraTextScOtf from "../../assets/fonts/BravuraTextSC.otf"
-// @ts-ignore
-import bravuraTextScWoff from "../../assets/fonts/BravuraTextSC.woff"
+
 import {BRAVURA_TEXT_SC, BRAVURA_TEXT_SC_OTF, BRAVURA_TEXT_SC_WOFF, WEB_APP_URL} from "./constants"
 
 const loadFonts = (): Promise<void> => {
-    return new Promise((resolve: () => void, reject: () => void): void => {
-        const localWoff = new FontFace(BRAVURA_TEXT_SC, `url('${bravuraTextScWoff}`)
+    return new Promise(async (resolve: () => void, reject: () => void): Promise<void> => {
+        // @ts-ignore
+        const localWoffUrl = await import(`../../assets/fonts/BravuraTextSC.woff`)
+        const localWoff = new FontFace(BRAVURA_TEXT_SC, `url('${localWoffUrl.default}`)
         localWoff.load().then((): void => {
             document.fonts.add(localWoff)
             resolve()
-        }).catch((err: Error): void => {
+        }).catch((): void => {
             const remoteWoff = new FontFace(BRAVURA_TEXT_SC, `url('${WEB_APP_URL}/${BRAVURA_TEXT_SC_WOFF}')`)
             remoteWoff.load().then((): void => {
                 document.fonts.add(remoteWoff)
@@ -21,7 +20,9 @@ const loadFonts = (): Promise<void> => {
             })
         })
 
-        const localOtf = new FontFace(BRAVURA_TEXT_SC, `url('${bravuraTextScOtf}`)
+        // @ts-ignore
+        const localOtfUrl = await import(`../../assets/fonts/BravuraTextSC.otf`)
+        const localOtf = new FontFace(BRAVURA_TEXT_SC, `url('${localOtfUrl.default}`)
         localOtf.load().then((): void => {
             document.fonts.add(localOtf)
             resolve()
