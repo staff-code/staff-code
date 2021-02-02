5c4d06585ab92125cf7dd307b98320a8b55905b1
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2021-02-02 10:58:16 -0800

    v1.0.204: GA & SMuFL 1.392

--- app/index.ts
+++ app/index.ts
@@ -1,5 +1,20 @@
 import {loadFonts, setupPackageRoot} from "../src"
 
+const googleAnalyticsTag1 = document.createElement("script")
+googleAnalyticsTag1.async = true
+googleAnalyticsTag1.src = "https://www.googletagmanager.com/gtag/js?id=G-QY3J3V3JGZ"
+document.head.appendChild(googleAnalyticsTag1)
+
+const googleAnalyticsTag2 = document.createElement("script")
+googleAnalyticsTag2.innerHTML = `
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+
+gtag('config', 'G-QY3J3V3JGZ');
+`
+document.head.appendChild(googleAnalyticsTag2)
+
 loadFonts().then((): void => {
     const root: HTMLSpanElement = setupPackageRoot()
     document.body.appendChild(root)
