dfb16bbe0289d31710cee0181f216846858733b3
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2021-02-03 11:25:34 -0800

    extract to-dos

--- src/ui/variants/package/index.ts
+++ src/ui/variants/package/index.ts
@@ -4,8 +4,3 @@ export {downloadSvg} from "./panel"
 // TODO: CLEAN, READY TO GO: TEST MORE GUI HELPER METHODS
 //  You should just go through everything that looks unit testable and backfill them
 //  `panel/input`, `panel/spinners`, and `reference/*` is what remains to be done
-
-// TODO: FEATURE IMPROVE, BLOCKED: HELPER VIDEO
-//  Could do. Wouldn't have to be more than a minute. Longest part might be going through the commands.
-//  Maybe Dave has some thoughts or feedback on it? Maybe we should write a script together? Or I'll just take a crack
-//  - Blocked on Dave's feedback about the script (though this really wouldn't be handled in the code anyway)
