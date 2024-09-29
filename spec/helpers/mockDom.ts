13892fea63afccd9bb07bbc52550c3c9d90ef0e4
Douglas Blumeyer <douglas@mechanical-orchard.com>
2024-09-29 00:40:53 -0700

    update visual specs

--- spec/helpers/mockDom.ts
+++ spec/helpers/mockDom.ts
@@ -6,7 +6,6 @@ const mockDom = ({url}: {url?: Link} = {url: WEB_APP_URL}): void => {
     globalThis.window = new JSDOM("", {url}).window as unknown as Window & typeof globalThis
 
     globalThis.document = globalThis.window.document
-    globalThis.navigator = globalThis.window.navigator
     globalThis.history = globalThis.window.history
 }
 
@@ -15,8 +14,6 @@ const unmockDom = (): void => {
     globalThis.window = undefined
     // @ts-ignore
     globalThis.document = undefined
-    // @ts-ignore
-    globalThis.navigator = undefined
     // @ts-ignore
     globalThis.history = undefined
 }
